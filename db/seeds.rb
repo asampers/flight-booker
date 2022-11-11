67cf6ed3a69268008e7817d315c579e51dc56938
nanarama <annasampers@gmail.com>
2022-11-11 16:32:46 -0600

    Create Airport model, migrate and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.find_or_create_by(code: "SFO", location: "San Francisco, CA")
+Airport.find_or_create_by(code: "NYC", location: "New York City, NY")
+Airport.find_or_create_by(code: "ATL", location: "Atlanta, GA")
+Airport.find_or_create_by(code: "ORD", location: "Chicago, IL")
+Airport.find_or_create_by(code: "LAX", location: "Los Angeles, CA")
+Airport.find_or_create_by(code: "DFW", location: "Dallas, TX")
+Airport.find_or_create_by(code: "MCO", location: "Orlando, FL")
+Airport.find_or_create_by(code: "DEN", location: "Denver, CO")
+
+puts 'Created 8 airports'
+
+#start = Time.zone.today
+#finish = Time.zone.today + 31.days
