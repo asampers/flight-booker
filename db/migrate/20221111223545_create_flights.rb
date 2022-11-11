c3dc0136639f1d44babb0000347d0adc15baae13
nanarama <annasampers@gmail.com>
2022-11-11 16:44:12 -0600

    Create Flight model, set associations with Airport, and migrate

---
+++ db/migrate/20221111223545_create_flights.rb
@@ -0,0 +1,13 @@
+class CreateFlights < ActiveRecord::Migration[7.0]
+  def change
+    create_table :flights do |t|
+      t.integer :origin_id
+      t.integer :destination_id
+      t.date :departure_date
+      t.time :departure_time
+      t.integer :duration
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_215712) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_223545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.integer "origin_id"
+    t.integer "destination_id"
+    t.date "departure_date"
+    t.time "departure_time"
+    t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
