11a05d726d22b3d4d6aac410e1f4d03fa45c229b
nanarama <annasampers@gmail.com>
2022-12-08 12:47:21 -0600

    Confirmation_email views

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -3,10 +3,12 @@ class PassengerMailer < ApplicationMailer
 
   def confirmation_email
     @booking = Booking.find(params[:id])
+    @flight = Flight.find(params[:booking][:flight_id])
     @passengers = @booking.passengers
-    
+    @url = root_url
+
     @passengers.each do |passenger|
-      mail(to: passenger.email, subject: "Confirmation of Flight# #{@booking.flight_id}")
+      mail(to: passenger.email, subject: "Confirmation of Flight# #{@flight.id}")
     end  
   end
 end
