bd45ed22c5480fe6c8f1d5a84ab3343c24e0a2b2
nanarama <annasampers@gmail.com>
2022-12-08 12:30:40 -0600

    Confirmation_email method setup

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,3 +1,12 @@
 class PassengerMailer < ApplicationMailer
-  
+  default from: 'notification@example.com'
+
+  def confirmation_email
+    @booking = Booking.find(params[:id])
+    @passengers = @booking.passengers
+    
+    @passengers.each do |passenger|
+      mail(to: passenger.email, subject: "Confirmation of Flight# #{@booking.flight_id}")
+    end  
+  end
 end
